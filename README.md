161fb5bc50025de76108c5bac892a1b6cc0ba0e0
Kevin Brebanov <kevin.brebanov@gmail.com>
2015-11-17 14:02:25 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Atlantic.net Python Bindings
+
+For definitive documentation on how the api works please visit:
+https://www.atlantic.net/docs/api/
+
+## License
+
+This code is provided under an MIT-style license. Please refer to the LICENSE
+file in the root of the project for specifics.
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = "atlantic-python",
+    version = "0.1",
+    description = "Python bindings for Atlantic.net API",
+    author = "Kevin Brebanov",
+    author_email = "kevin.brebanov@gmail.com",
+    url = "https://github.com/kbrebanov/atlantic-python",
+    packages = ['atlantic'],
+    extras_require = {},
+)
